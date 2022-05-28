df524f6dafc280bbb4acaf6ed2c387aff8511dc5
spaquette <spaquette@octolabs.io>
2022-05-28 15:43:47 -0400

    update second test

--- tests/test_broker_score.py
+++ tests/test_broker_score.py
@@ -1,8 +1,11 @@
-from community_broker_score import detect_brokering_edges, detect_community_brokers
+from community_broker_score import (
+    detect_brokering_edges,
+    detect_community_brokers
+    )
 import pandas as pd
 
 
-def test_detect_brokering_edges_returns_dataframe():
+def test_detect_brokering_edges_returns():
 
     #  We create 2 brokering edges, (1,2) and (5,1)
     nodes = pd.DataFrame({
@@ -16,11 +19,27 @@ def test_detect_brokering_edges_returns_dataframe():
     result = detect_brokering_edges(nodes, edges)
 
     assert type(result) == pd.DataFrame
-    assert result.columns == ['A', 'B', 'community_id_A', 'community_id_B', 'community_broker']
-
+    assert 'community_broker' in result.columns
     assert len(result) == 2
 
     assert result.iloc[0]['A'] == 1
     assert result.iloc[0]['B'] == 2
     assert result.iloc[1]['A'] == 5
     assert result.iloc[1]['B'] == 1
+
+def test_detect_community_brokers_returns():
+
+    #  We create 2 brokering edges, (1,2) and (5,1)
+    nodes = pd.DataFrame({
+    'id': [1, 2, 3, 4, 5],
+    'community_id': ['a', 'b', 'c', 'c', 'b']}
+    )
+    edges = pd.DataFrame({
+    'A': [1, 2, 3, 4, 5],
+    'B': [2, 5, 4, 3, 1]}
+    )
+    result = detect_community_brokers(nodes, edges)
+    print(result)
+    assert type(result) == pd.DataFrame
+    assert 'n_community_broker' in result.columns
+    assert False
